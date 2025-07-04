5f3479e3b7d07d7db316f4bd5bdb61969b5a1a4e
Leodair <163547863+leodair@users.noreply.github.com>
2025-07-04 07:34:18 -0300

    Add files via upload

---
+++ ecommerce/produtos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="loja.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>produtos</title>
+</head>
+<body>
+    
+<nav class="navbar">
+    <div class="logo">LEODAIR_CONSTRU</div>
+    <ul class="nav-links">
+        <li><a href="dashboard.php">Início</a></li>
+        <li><a href="produtos.php">Produtos</a></li>
+        <li><a href="carrinho.php">Carrinho</a></li>
+        <li><a href="contato.php">Contato</a></li>
+        <li><a href="login.php">Login</a></li>
+    </ul>
+    </nav>
+
+    
+</body>
+</html>
